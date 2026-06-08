b96ee4026e12a88b4cfe5460136f40a20c0c167d
ctrotech-tutor <ctrotech.devs@gmail.com>
2026-06-08 18:49:58 +0100

    chore: reset to v1.0.0 for fresh start

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ export * from './core/validate';
 export * from './rules';
 export * as rules from './rules';
 export * from './i18n/Translator';
-export const version = '4.0.0';
+export const version = '1.0.0';
